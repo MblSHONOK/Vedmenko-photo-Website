2fbc749b72f59c5f953bae9cadb9e348fcc254ee
Ilia Vedmenko <106385207+MblSHONOK@users.noreply.github.com>
2022-08-23 15:45:00 +0300

    Add files via upload

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('portfolio', views.portfolio, name='portfolio'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('portrait-photography', views.portrait_photography, name='portrait-photography'),
+    path('report-photography', views.report_photography, name='report-photography'),
+    path('genre-photography', views.genre_photography, name='genre-photography'),
+    path('street-photography', views.street_photography, name='street-photography'),
+    path('bw-photography', views.bw_photography, name='bw-photography'),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from taskmanager.settings import RECIPIENTS_EMAIL, DEFAULT_FROM_EMAIL
+
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
@@ -11,4 +17,36 @@ def about(request):
     return render(request, 'main/about.html')
 
 def contact(request):
-    return render(request, 'main/contact.html')
+    if request.method == 'GET':
+        form = ContactForm()
+    elif request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['name']
+            subject2 = form.cleaned_data['lastname']
+            sender = form.cleaned_data['sender']
+            subject3 = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(f'{subject}_{subject2}', f'тема: {subject3}\n сообщение: {message} от {sender} ', DEFAULT_FROM_EMAIL, RECIPIENTS_EMAIL)
+            except BadHeaderError:
+                return HttpResponse('Ошибка в теме письма.')
+            return HttpResponseRedirect('contact')
+    else:
+        return HttpResponse('Неверный запрос.')
+    return render(request, 'main/contact.html', {'form': form})
+
+def portrait_photography(request):
+    return render(request, 'main/portrait-photography.html')
+
+def report_photography(request):
+    return render(request, 'main/report-photography.html')
+
+def genre_photography(request):
+    return render(request, 'main/genre-photography.html')
+
+def street_photography(request):
+    return render(request, 'main/street-photography.html')
+
+def bw_photography(request):
+    return render(request, 'main/bw-photography.html')
