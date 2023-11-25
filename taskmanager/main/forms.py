e5da7c77e97dc969b49d8f63b8e3f030bce28346
regidf12 <tiltedfear@gmail.com>
2023-11-25 18:43:01 +0300

    edit setting and links

--- taskmanager/main/forms.py
+++ taskmanager/main/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     lastname = forms.CharField(max_length=100)
     sender = forms.EmailField(max_length=100)
     subject = forms.CharField(max_length=100)
-    message = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea, required=True)
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from taskmanager.settings import RECIPIENTS_EMAIL, DEFAULT_FROM_EMAIL
+from django.views.generic import ListView
 
-# Create your views here.
+
 def index(request):
     return render(request, 'main/index.html')
+
 
 def portfolio(request):
     return render(request, 'main/portfolio.html')
 
+
 def about(request):
     return render(request, 'main/about.html')
+
 
 def contact(request):
     if request.method == 'GET':
@@ -28,7 +31,8 @@ def contact(request):
             subject3 = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             try:
-                send_mail(f'{subject}_{subject2}', f'тема: {subject3}\n сообщение: {message} от {sender} ', DEFAULT_FROM_EMAIL, RECIPIENTS_EMAIL)
+                send_mail(f'{subject}_{subject2}', f'тема: {subject3}\n сообщение: {message} от {sender} ',
+                          DEFAULT_FROM_EMAIL, RECIPIENTS_EMAIL)
             except BadHeaderError:
                 return HttpResponse('Ошибка в теме письма.')
             return HttpResponseRedirect('contact')
@@ -36,17 +40,22 @@ def contact(request):
         return HttpResponse('Неверный запрос.')
     return render(request, 'main/contact.html', {'form': form})
 
+
 def portrait_photography(request):
     return render(request, 'main/portrait-photography.html')
+
 
 def report_photography(request):
     return render(request, 'main/report-photography.html')
 
+
 def genre_photography(request):
     return render(request, 'main/genre-photography.html')
 
+
 def street_photography(request):
     return render(request, 'main/street-photography.html')
+
 
 def bw_photography(request):
     return render(request, 'main/bw-photography.html')
